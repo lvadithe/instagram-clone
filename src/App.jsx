605a489006d7b5c7561b3843bdf151380c37557a
vadith gomez <legrone@MacBook-Pro.local>
2022-03-23 19:33:23 -0500

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './components/Principal/Post/Post';
 import LOGO from "./assets/insta_logo.png";
 import './App.css';
 
 function App() {
+  const [post, setPost] = useState([])
+
   return (
     <div className="app">
       <div className="app__header">
@@ -13,8 +15,12 @@ function App() {
           alt=""
         />
       </div>
-      <Post />
-      
+      <Post username="Legrone" caption="WOW day three" imageUrl="https://www.onlydogs.info/wp-content/uploads/2016/04/Doberman01ok-1000x1000.jpg" />
+      <Post username="asdfhlkd" caption="WOW day asdasdas" imageUrl="https://a3.cdn.japantravel.com/photo/3016-18546/738x553.5!/tokyo-shibuya-crossing-18546.jpg" />
+      <Post username="sdffv" caption="WOW dsadas three" imageUrl="https://wortev.capital/wp-content/uploads/2021/07/principales-paises-que-mas-invierten-en-criptomonedas.jpg.webp" />
+      <Post username="sddffv" caption="WOW fdsfff three" imageUrl="https://concepto.de/wp-content/uploads/2019/04/Nebulosa-de-Orion-e1554813166944.jpg" />
+      <Post username="sfrr" caption="WOW day fds" imageUrl="https://cdn.pixabay.com/photo/2020/04/04/15/07/sushi-5002639_1280.jpg" />
+
     </div>
   )
 }
--- src/components/Principal/Post/Post.jsx
+++ src/components/Principal/Post/Post.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import LOGO from '../../../assets/reactjs.png';
+import Avatar from '@material-ui/core/Avatar'
 import "./Post.css";
 
-function Post() {
+function Post({username, caption, imageUrl}) {
   return (
-    <div className='post' >
-      <h3>Username</h3>
+    <div className='post'>
+      <div className="post__header">
+        <Avatar
+          className='post__avatar'
+          alt='Legrone'
+          src='../../../assets/Me-e.jpeg'
+        />
+        <h3>{username}</h3>
+      </div>
       {/* header => avatar + username */}
-      <img className='post__image' src={LOGO} alt="" />
+      <img className='post__image' src={imageUrl} alt="" />
       {/* image */}
 
-      <h4 className='post__text'><strong>cleverqazi</strong> WOW day three</h4>
+      <h4 className='post__text'><strong>{username}</strong> {caption}</h4>
       {/* username + caption */}
     </div>
   )
