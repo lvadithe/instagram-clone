d9174b864a2e46234aad75bf62fe2a8c0720bcda
vadith gomez <legrone@MacBook-Pro.local>
2022-03-27 15:48:26 -0500

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Posts from './components/Principal/Posts/Posts';
 import LOGO from "./assets/insta_logo.png";
 import { Input, Modal } from '@material-ui/core';
@@ -6,6 +6,7 @@ import { Box } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import './App.css';
 import Nav from './components/Secondary/Nav/Nav';
+import { auth } from './FireBase/firebase';
 
 const style = {
   position: 'absolute',
@@ -21,45 +22,118 @@ const style = {
 
 function App() {
   const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+
+
+  /* Important */
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        setUser(null)
+      }
+    })
+    return () => {
+      unsubscribe()
+    }
+  }, [user, username])
 
   const signUp = (event) => {
+    event.preventDefault()
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username
+        });
+      })
+      .catch((error) => alert(error.message))
+  }
 
+  const signIn = (event) => {
+    event.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
+
+    setOpenSignIn(false)
   }
 
   return (
     <div className="app">
-      <Modal open={open} onClose={() => setOpen(false)}>
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
         <Box sx={style}>
-          <img
-            className="app__headerImage"
-            src={LOGO}
-            alt=""
-          />
-          <Input
-            type='text'
-            placeholder='username'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <Input
-            type='text'
-            placeholder='email'
-            value={username}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            type='text'
-            placeholder='password'
-            value={username}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <form className='app__signUp' >
+            <img
+              className="app__headerImage"
+              src={LOGO}
+              alt=""
+            />
+            <Input
+              type='text'
+              placeholder='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              type='password'
+              placeholder='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signIn}>Sign In</Button>
+          </form>
         </Box>
       </Modal>
+
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Box sx={style}>
+          <form className='app__signUp' >
+            <img
+              className="app__headerImage"
+              src={LOGO}
+              alt=""
+            />
+            <Input
+              type='text'
+              placeholder='username'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              type='text'
+              placeholder='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              type='password'
+              placeholder='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signUp}>Sign Up</Button>
+          </form>
+        </Box>
+      </Modal>
+      {
+        user ? (
+          <Button onClick={() => auth.signOut()}>Logout</Button>
+        ) : (
+          <div className="app__login__container">
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(true)}>Sign up</Button>
+          </div>
+        )
+      }
       <Nav />
-      <Button onClick={() => setOpen(true)}>Sign up</Button>
+
       <Posts />
     </div>
   )
